f0193ac95ac5a8c83706f0f3b1d3201991e49010
Md Zunaed A. Tanim <56795901+zunaed-tanim@users.noreply.github.com>
2020-09-16 23:20:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Name              : Md Zunaed Tanim
 # Lab               : 2 / Expressions
 # Date Created      : 14 September, 2020
-# Git Repository    :
+# Git Repository    : https://github.com/zunaed-tanim/ETGG_1801/blob/Lab-02/main.py
 
 # import Pillow library
 import PIL
