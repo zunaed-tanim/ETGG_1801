7a4f005318b254d12a00aabfd5bf55b1585fb944
Md Zunaed A. Tanim <56795901+zunaed-tanim@users.noreply.github.com>
2020-12-28 15:45:51 +0600

    Update MdZunaed_TanimLab02.py
    ASCII to image conversation code part taken out

--- MdZunaed_TanimLab02.py
+++ MdZunaed_TanimLab02.py
@@ -1,13 +1,10 @@
+"""
 # Course ID-Section : ETGG1801.90
 # Name              : Md Zunaed Tanim
 # Lab               : 2 / Expressions
 # Date Created      : 16 September, 2020
-# Git Repository    : https://github.com/zunaed-tanim/ETGG_1801/blob/Lab-02/MdZunaed_TanimLab02.py
-
-# import Pillow library
-import PIL.Image
-
-# ------------------ STEP 03 ------------------
+# Git Repository    : https://github.com/zunaed-tanim/ETGG_1801.git
+"""
 
 # Drawing a coffee cup ASCII art with multiple print statements
 print("\n      \'   \'   \'")
@@ -23,8 +20,6 @@ print("   /              /   /")
 print("  /              /===/")
 print("  \\_____________/")
 
-
-# ------------------ STEP 04 ------------------
 
 # Drawing a coffee cup ASCII art with a single print statement
 print('''
@@ -42,9 +37,6 @@ _______________________
   \\_____________/
 
 ''')
-
-
-# ------------------ STEP 05 ------------------
 
 
 # Ask user to enter ASCII health bar art characters for single health unit
@@ -78,75 +70,7 @@ def health_check():
 # Call the function
 health_check()
 
-# ------------------ STEP 06, 07, 08 ------------------
-
 # Halt the program until the user presses enter
 input("Press enter to continue...")
-
-# ***-- The program below prints ASCII image from an image file --*** #
-
-# Define list of ASCII characters
-ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
-
-
-# resize image according to a new width
-def resize_image(image, new_width=100):
-    width, height = image.size
-    ratio = height / width / 1.65
-    new_height = int(new_width * ratio)
-    resized_image = image.resize((new_width, new_height))
-    return resized_image
-
-
-# convert each pixel to grayscale
-def converted_grayscale(image):
-    grayscale_image = image.convert("L")
-    return grayscale_image
-
-
-# convert pixels to a string of ascii characters
-def ascii_from_pixels(image):
-    pixels = image.getdata()
-    characters = "".join([ASCII_CHARS[pixel // 25] for pixel in pixels])
-    return characters
-
-
-def main_ascii(new_width=100):
-    # approach to open image from user-input
-    path = input("Enter a valid pathname to an image: ")
-    try:
-        image = PIL.Image.open(path)
-    except:
-        print(path, " -is an invalid pathname to an image. Please try again")
-        return
-
-    # convert image to ascii
-    new_image_data = ascii_from_pixels(converted_grayscale(resize_image(image)))
-
-    # format
-    pixel_count = len(new_image_data)
-    ascii_image = "\n".join([new_image_data[index:(index + new_width)] for index in range(0, pixel_count, new_width)])
-
-    # print result
-    print(ascii_image)
-
-    # save result to "ascii_image.txt"
-    with open("ascii_image.txt", "w") as f:
-        f.write(ascii_image)
-
-
-# run program
-main_ascii()
-
-
-
-"""
-* Note: When I was researching ASCII art, I came across the option to convert image to ASCII art, wanted to try it.
-        I found different approaches, but I liked the cleanliness and ease of understanding of the above approach.
-        I slightly modified the code but followed along mostly from the original sources (See below) 
-        since I liked it as it is. Didn't feel the need to modify too much. 
-        Sources: https://github.com/kiteco/python-youtube-code/commit/663dc86d8bcab7d3f1d0e5388771ea7889994fc5,
-        https://medium.com/javascript-dots/python-ascii-art-generator-60ba9eb559d7 
-"""
 
 
